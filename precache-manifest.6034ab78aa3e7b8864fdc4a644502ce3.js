6b4b44e4ff4bb0426dfeeb8543e6c241a1d8c444
praful aparnathi <aparnathiprafulk758@gmail.com>
2019-11-17 22:52:14 -0800

    Updates

---
+++ precache-manifest.6034ab78aa3e7b8864fdc4a644502ce3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2160d828785e96a67ccd",
+    "url": "/test/static/js/runtime~main.eded8796.js"
+  },
+  {
+    "revision": "46c79ee6dacd8fc3211b",
+    "url": "/test/static/js/main.1c42b4ee.chunk.js"
+  },
+  {
+    "revision": "d499d87a993599493776",
+    "url": "/test/static/js/2.1e4a2dbe.chunk.js"
+  },
+  {
+    "revision": "46c79ee6dacd8fc3211b",
+    "url": "/test/static/css/main.81a27b41.chunk.css"
+  },
+  {
+    "revision": "1fc9814c78f5286ddfc4651da31fa10f",
+    "url": "/test/index.html"
+  }
+];
